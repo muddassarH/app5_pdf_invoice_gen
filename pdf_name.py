144ecf1d8cddd13dd4e9da702ef91e69b91281dc
muddassarH <muddassarhussain90@gmail.com>
2023-05-09 16:17:18 +0500

    Create pdf of names of txt files

--- pdf_name.py
+++ pdf_name.py
@@ -1,8 +1,7 @@
-import pandas as pd
 from fpdf import FPDF
 from pathlib import Path
 import glob
-
+import time
 filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
@@ -11,5 +10,7 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     name = filename.title()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt= name, ln=1)
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+    time.time()
 pdf.output("output.pdf")
+
