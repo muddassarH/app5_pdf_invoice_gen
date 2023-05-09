d61e3012c52c1339727f20b37173040dacb638f7
muddassarH <muddassarhussain90@gmail.com>
2023-05-09 16:43:57 +0500

    Create pdf of names of txt files

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date:.{date}")
+
     pdf.output(f"Pdfs/{filename}.pdf")
